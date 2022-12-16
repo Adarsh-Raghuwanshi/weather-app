bfafed60ec163b77456c9004c60d8e697e5055a3
Adarsh Raghuwanshi <adarsh.raghuwanshi184@gmail.com>
2022-12-16 11:17:55 +0530

    hide the apiKey

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ let weatherImg = document.querySelector("#desc img");
 let time = document.querySelector("#time");
 let date = document.querySelector("#date");
 
-let months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let key, months, days;
 let url = "http://api.openweathermap.org/data/2.5/weather?q=";
-let key = "8edd671f81db81163cbd706f126805d5";
+fetch("./file.json").then(response => response.json())
+.then(json => {
+    key = json.key;
+    months = json.months;
+    days = json.days;
+});
 
 btn.addEventListener("click", searchCityWeather);
 
